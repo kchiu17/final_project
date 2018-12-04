c442c5497ea5e036aaedebddcb7dd59e2475deda
Kevin Chiu <kchiu17@uw.edu>
2018-12-03 16:04:56 -0800

    More Widgets

--- scripts/server.R
+++ scripts/server.R
@@ -18,7 +18,12 @@ server <- function(input, output) {
   output$plot <- renderPlot({
     artist_info <- data() %>%
       filter(tempo >= input$tempo[1], tempo <= input$tempo[2]) %>%
-      filter(danceability >= input$danceability[1], danceability <= input$danceability[2])
+      filter(danceability >= input$danceability[1], 
+             danceability <= input$danceability[2]) %>%
+      filter(album_release_date >= input$album_release_date[1],
+             album_release_date <= input$album_release_date[2]) %>%
+      filter((duration_ms / 60000) >= input$duration[1],
+             (duration_ms / 60000) <= input$duration[2])
     
     plot<-ggplot(artist_info, aes(x=track_name, y=track_popularity)) +
       geom_bar(stat="identity") + coord_flip()
--- scripts/ui.R
+++ scripts/ui.R
@@ -6,8 +6,12 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       textInput("artist", "Input Artist Here", "Taylor Swift"),
+      dateRangeInput("album_release_date", "Date Released", start=Sys.Date()-1825
+                     , end=Sys.Date(), format = "yyyy-mm-dd"),      
       sliderInput("tempo", "Tempo", 0, 200, c(80,100), step=10),
-      sliderInput("danceability", "Danceability", 0, 1, c(0.5,0.6), step=0.05)
+      sliderInput("danceability", "Danceability", 0, 1, c(0.5,0.6), step=0.05),
+      sliderInput("duration", "Duration (minutes)", 0, 10, c(2,3), step=.25)
+      
       ),
     mainPanel(
       div(style="text-align:center;font-size:1.8em", textOutput("title_graph")),
